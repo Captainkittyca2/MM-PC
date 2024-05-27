8a67a4af55dd93bb2c782e90fb034c77bec7985a
Captain Kitty Cat <68467449+Captainkittyca2@users.noreply.github.com>
2024-05-27 10:30:24 +0300

    Add files via upload

---
+++ mm/windows/properties.h
@@ -0,0 +1,14 @@
+
+#define VER_FILEVERSION 1, 0, 0, 0
+#define VER_FILEVERSION_STR "1.0.0\0"
+
+#define VER_PRODUCTVERSION 1, 0, 0, 0
+#define VER_PRODUCTVERSION_str "1.0.0\0"
+
+#define VER_COMPANYNAME_STR "github.com/harbourmasters\0"
+#define VER_PRODUCTNAME_STR "2 Ship 2 Harkinian\0"
+
+#define VER_INTERNALNAME_STR "2ship\0"
+#define VER_ORIGINALFILENAME_STR "2ship.exe\0"
+
+#define VER_FILEDESCRIPTION_STR "2 Ship 2 Harkinian - Rika Alfa\0"
